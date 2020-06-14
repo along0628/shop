45dabcf22ee17841db0a370397ee1c8a5ffdb2ef
along0628 <506570320@qq.com>
2020-06-14 18:24:55 +0800

    update idea to forst commit.

--- src/main/java/com/shop/shop/ShopApplication.java
+++ src/main/java/com/shop/shop/ShopApplication.java
@@ -2,9 +2,10 @@ package com.shop.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.shop.controller"})
 public class ShopApplication {
 
     public static void main(String[] args) {
